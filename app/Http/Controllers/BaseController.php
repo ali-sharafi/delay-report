89c621d7cc9fc3446653c037ecc469f455b387a0
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-26 22:25:45 +0430

    Add comment & const to basecontroller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BaseController extends Controller
 {
+    /**
+     * @var
+     * 
+     * Responses statuses
+     */
+    const SUCCESS = 'Success',
+        ERROR = 'Error',
+        ERROR_MESSAGE = 'Some Error occured';
+
     public function __construct(protected Request $request)
     {
     }
 
-    public function successReponse($status = 'Success', $data = [], $statusCode = 200)
+    /**
+     * Send a success response to client
+     * 
+     * @param static::SUCCESS $status
+     * @param array $data
+     * @param \Illuminate\Http\Response::HTTP_OK
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function successReponse($status = self::SUCCESS, $data = [], $statusCode = Response::HTTP_OK)
     {
         return response()->json([
             'status' => $status,
@@ -18,7 +37,16 @@ class BaseController extends Controller
         ], $statusCode);
     }
 
-    public function errorResponse($status = 'Error', $message = 'Some Error occured', $statusCode = 400)
+    /**
+     * Send a success response to client
+     * 
+     * @param static::ERROR $status
+     * @param string $message
+     * @param \Illuminate\Http\Response::HTTP_BAD_REQUEST
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function errorResponse($status = self::ERROR, $message = self::ERROR_MESSAGE, $statusCode = Response::HTTP_BAD_REQUEST)
     {
         return response()->json([
             'status' => $status,
