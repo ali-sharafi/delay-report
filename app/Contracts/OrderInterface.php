30184e018cb2ba6bb0b0eba4d58674e2b4940a7e
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-26 22:16:33 +0430

    Add findOrderDelayTime to OrderInterface

--- app/Contracts/OrderInterface.php
+++ app/Contracts/OrderInterface.php
@@ -7,4 +7,6 @@ use App\Models\Order;
 interface OrderInterface
 {
     public function canAddDelayReportToOrder(Order $order): bool;
+
+    public function findOrderDelayTime(Order $order): mixed;
 }
