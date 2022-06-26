1c2071e7fbc6c67cc5ea58ac5949f8eb6be8e51b
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-26 22:05:20 +0430

    Add success & error response to basecontroller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -9,4 +9,20 @@ class BaseController extends Controller
     public function __construct(protected Request $request)
     {
     }
+
+    public function successReponse($status = 'Success', $data = [], $statusCode = 200)
+    {
+        return response()->json([
+            'status' => $status,
+            'data' => $data
+        ], $statusCode);
+    }
+
+    public function errorResponse($status = 'Error', $message = 'Some Error occured', $statusCode = 400)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+        ], $statusCode);
+    }
 }
