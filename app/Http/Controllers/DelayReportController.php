879c2b55a78c70124f3e801b890cb857cebf4d9e
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-27 19:07:20 +0430

    Complete create delay report

--- app/Http/Controllers/DelayReportController.php
+++ app/Http/Controllers/DelayReportController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Services\DelayReportService;
 use App\Services\OrderService;
 
 class DelayReportController extends BaseController
 {
-    public function store(Order $order, OrderService $orderService)
+    public function store(Order $order, OrderService $orderService, DelayReportService $delayReportService)
     {
         if ($orderService->isNeedNewDelayTime($order)) {
             $deliveryTime = $orderService->findOrderDelayTime($order);
@@ -15,5 +16,9 @@ class DelayReportController extends BaseController
             $message = "Your order will devliver at $deliveryTime";
             return $this->successReponse(static::SUCCESS, $message);
         }
+
+        $delayReportService->create($order);
+
+        return $this->successReponse();
     }
 }
