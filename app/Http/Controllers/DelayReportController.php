09c290d3cedcc7bf67767b732a41ee3e15369359
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-27 20:00:12 +0430

    Refactor DelayReportController

--- app/Http/Controllers/DelayReportController.php
+++ app/Http/Controllers/DelayReportController.php
@@ -4,21 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Services\DelayReportService;
-use App\Services\OrderService;
 
 class DelayReportController extends BaseController
 {
-    public function store(Order $order, OrderService $orderService, DelayReportService $delayReportService)
+    public function store(Order $order, DelayReportService $delayReportService)
     {
-        if ($orderService->isNeedNewDelayTime($order)) {
-            $deliveryTime = $orderService->findOrderDelayTime($order);
+        $response = $delayReportService->create($order);
 
-            $message = "Your order will devliver at $deliveryTime";
-            return $this->successReponse(static::SUCCESS, $message);
-        }
-
-        $delayReportService->create($order);
-
-        return $this->successReponse();
+        return $this->successReponse(static::SUCCESS, ['message' => $response]);
     }
 }
