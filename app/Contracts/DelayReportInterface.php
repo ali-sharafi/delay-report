50d9138d839410f3a2b3bbf3a114bcfb73712e10
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-27 20:25:17 +0430

    Add Redis delay key & assign function to delayreportInterface

--- app/Contracts/DelayReportInterface.php
+++ app/Contracts/DelayReportInterface.php
@@ -6,7 +6,10 @@ use App\Models\Order;
 
 interface DelayReportInterface
 {
+    const REDIS_DELAY_KEY = 'delay_report_list';
+
     public function create(Order $order): string;
     public function createNewOrderDeliveryTime(Order $order): int;
     public function addDelayReortToQueue(Order $order): void;
+    public function assignDelayToAgent(): mixed;
 }
