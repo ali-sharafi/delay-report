162e7361ffb57e9f3978cd7fe39ad222d640558b
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-27 20:27:01 +0430

    Add assign function to delayReportController & refactor its service

--- app/Http/Controllers/DelayReportController.php
+++ app/Http/Controllers/DelayReportController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Services\DelayReportService;
+use Illuminate\Http\Request;
 
 class DelayReportController extends BaseController
 {
-    public function store(Order $order, DelayReportService $delayReportService)
+    public function __construct(protected Request $request, protected DelayReportService $delayReportService)
     {
-        $response = $delayReportService->create($order);
+        parent::__construct($request);
+    }
+
+    public function store(Order $order)
+    {
+        $response = $this->delayReportService->create($order);
 
         return $this->successReponse(static::SUCCESS, ['message' => $response]);
+    }
+
+    public function assign()
+    {
+        $response = $this->delayReportService->assignDelayToAgent();
+
+        return $this->successReponse($response);
     }
 }
