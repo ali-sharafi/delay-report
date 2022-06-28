ecd6249daa2376436858844d6969107534dae687
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-28 19:58:19 +0430

    Add agent busy translation

--- lang/en/delay_report.php
+++ lang/en/delay_report.php
@@ -4,5 +4,6 @@ return [
     'new_delivery_time' => 'Your order will devliver at :time',
     'delay_report_created' => 'Your Delay report created successfully',
     'delivery_time_not_expired' => 'Delivery time not expired yet',
-    'order_in_queue_already' => 'Your Order added to process queue already.'
+    'order_in_queue_already' => 'Your Order added to process queue already.',
+    'agent_has_order' => 'You have an active order already.'
 ];
