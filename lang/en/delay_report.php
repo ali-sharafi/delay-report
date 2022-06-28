ed6bb3e50f0899cf79dc47e83fd054bf9db5d4d5
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-28 08:51:36 +0430

    Add appripriate translate for delivery time error

--- lang/en/delay_report.php
+++ lang/en/delay_report.php
@@ -2,5 +2,6 @@
 
 return [
     'new_delivery_time' => 'Your order will devliver at :time',
-    'delay_report_created' => 'Your Delay report created successfully'
+    'delay_report_created' => 'Your Delay report created successfully',
+    'delivery_time_not_expired' => 'Delivery time not expired yet'
 ];
