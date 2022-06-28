b7fab57e1c96ab91bf339d6758f8e7d47fa42273
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-28 07:43:49 +0430

    Add datebetween scope to delayReport model

--- app/Models/DelayReport.php
+++ app/Models/DelayReport.php
@@ -10,4 +10,9 @@ class DelayReport extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function scopeOfDateBetween($query, string $startDate, string $endDate)
+    {
+        return $query->whereBetween('date_at', [$startDate, $endDate]);
+    }
 }
