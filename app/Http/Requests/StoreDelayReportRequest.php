d842c3c0a6992decd25f3a76bc1919fcee260b6a
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-28 09:01:50 +0430

    Complete StoreDelayReportRequest

---
+++ app/Http/Requests/StoreDelayReportRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\DeliveryTime;
+
+class StoreDelayReportRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->mergeIfMissing(['order' => $this->order]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'order' => ['required', new DeliveryTime()]
+        ];
+    }
+}
