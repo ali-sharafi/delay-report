0444d52c63db92eee592fd216ef50effc75f71f5
Ali Sharafi <a.sharafi1993@gmail.com>
2022-06-29 18:38:12 +0430

    Fix translation typo

--- lang/en/delay_report.php
+++ lang/en/delay_report.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'new_delivery_time' => 'Your order will devliver at :time',
+    'new_delivery_time' => 'Your order will deliver at :time',
     'delay_report_created' => 'Your Delay report created successfully',
     'delivery_time_not_expired' => 'Delivery time not expired yet',
     'order_in_queue_already' => 'Your Order added to process queue already.',
